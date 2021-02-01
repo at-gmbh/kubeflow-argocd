7086cc055173a2b8a07e727104c74908e74d286c
Karl Schriek <kschriek@gmail.com>
2021-02-01 10:25:54 +0100

    sleep at end

--- update.sh
+++ update.sh
@@ -1,9 +1,10 @@
 while :
 do
+    git pull
 	aws s3 cp s3://863518836478-read/argocd/applications  argocd/applications --recursive
     echo "sleeping"
-	sleep 60
     git commit -a -m "update"
     git push
+	sleep 60
 done
 
