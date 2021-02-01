88b7aee8056265d9e25ebc3185b618c7fc142a36
Karl Schriek <kschriek@gmail.com>
2021-02-01 10:25:10 +0100

    autocommit

--- update.sh
+++ update.sh
@@ -1,7 +1,9 @@
 while :
 do
 	aws s3 cp s3://863518836478-read/argocd/applications  argocd/applications --recursive
-    ehco("sleeping")
+    echo "sleeping"
 	sleep 60
+    git commit -a -m "update"
+    git push
 done
 
