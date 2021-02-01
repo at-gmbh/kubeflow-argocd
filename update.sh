17d2d9c7e5d752072c5cb903c987588e0c381e2e
Karl Schriek <kschriek@gmail.com>
2021-02-01 10:23:59 +0100

    while loop with sleep

--- update.sh
+++ update.sh
@@ -1,1 +1,7 @@
-aws s3 cp s3://863518836478-read/argocd/applications  argocd/applications --recursive
+while :
+do
+	aws s3 cp s3://863518836478-read/argocd/applications  argocd/applications --recursive
+    ehco("sleeping")
+	sleep 60
+done
+
